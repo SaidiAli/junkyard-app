434c8500ca7d808a3917f7c3e30bc48d39fb2e1f
bonstana <isikosaidiali@gmail.com>
2025-12-14 09:42:55 +0300

    update

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -9,8 +9,6 @@ export default function Providers({ children }: { children: ReactNode }) {
   const [queryClient] = useState(() => new QueryClient({
     defaultOptions: {
       queries: {
-        // With SSR, we usually want to set some default staleTime
-        // above 0 to avoid refetching immediately on the client
         staleTime: 60 * 1000,
       },
     },
